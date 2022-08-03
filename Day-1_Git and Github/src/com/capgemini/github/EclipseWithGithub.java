31e9e4e292305486090922ba9700286af397e019
Ritesh Mishra <96378085+iriteshmishra@users.noreply.github.com>
2022-08-03 09:34:13 +0530

    First Commit

---
+++ Day-1_Git and Github/src/com/capgemini/github/EclipseWithGithub.java
@@ -0,0 +1,10 @@
+package com.capgemini.github;
+//Demo to connect eclipse with github
+public class EclipseWithGithub {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome Ritesh Mishra.");
+
+	}
+
+}
