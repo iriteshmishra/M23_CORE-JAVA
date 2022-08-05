444bbf632a488251ec9aedf675f12fd34f43e673
Ritesh Mishra <96378085+iriteshmishra@users.noreply.github.com>
2022-08-06 00:09:12 +0530

    Second Commit

--- Day-2_Introduction to Java/src/com/capgemini/introduction/StringProgram.java
+++ Day-2_Introduction to Java/src/com/capgemini/introduction/StringProgram.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class StringProgram {
 
 	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
+        Scanner sc=new Scanner(System.in);
 		
 		String str = sc.next();
 		System.out.println(str);
