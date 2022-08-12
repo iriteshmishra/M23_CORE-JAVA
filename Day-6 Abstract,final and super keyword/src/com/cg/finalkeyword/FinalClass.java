f770b38b6a2e62b851b0cfbd23dbf2915caca940
Ritesh Mishra <96378085+iriteshmishra@users.noreply.github.com>
2022-08-12 23:51:41 +0530

    Second Commit

---
+++ Day-6 Abstract,final and super keyword/src/com/cg/finalkeyword/FinalClass.java
@@ -0,0 +1,34 @@
+package com.cg.finalkeyword;
+
+class Fruit
+{
+	//we can't make any constructor as final
+	//final Fruit()
+	Fruit()
+	{
+		System.out.println("Constructor");
+	}
+	//if you make any method as final you can't override it
+	//final public void accept()
+	public void accept()
+	{
+		System.out.println("Parent class");
+	}
+}
+class Apple extends Fruit
+{
+	public void accept()
+	{
+		System.out.println("Parent class");
+	}
+}
+
+public class FinalClass {
+
+	public static void main(String[] args) {
+		Apple a=new Apple();
+		a.accept();
+
+	}
+
+}
---
+++ Day-6 Abstract,final and super keyword/src/com/cg/finalkeyword/FinalVariable.java
@@ -0,0 +1,21 @@
+package com.cg.finalkeyword;
+
+public class FinalVariable {
+	final int FIN_VAR=45;
+	/*public void print()
+	 {
+		//if any variable is declare as a final we can't change the value of that variable
+		 
+		 FIN_VAR=76;
+	 }*/
+	public static void main(String[] args) {
+		 
+		 FinalVariable f=new FinalVariable();
+		 
+		 
+		 System.out.println(f.FIN_VAR);
+		 
+
+	}
+
+}
