3b9e721c7f239e80d59cc726f9ec42f4dbe18000
Ritesh Mishra <96378085+iriteshmishra@users.noreply.github.com>
2022-08-13 23:29:20 +0530

    First Commit

---
+++ Day-7_this,static and instanceof keyword/src/com/cg/statickeyword/StaticImportDemo.java
@@ -0,0 +1,15 @@
+package com.cg.statickeyword;
+
+//Math package imported using static
+import static java.lang.Math.*;
+public class StaticImportDemo {
+
+	public static void main(String[] args) {
+		System.out.println(sqrt(16));
+		System.out.println(ceil(12.23));
+		System.out.println(floor(12.23));
+
+
+	}
+
+}
