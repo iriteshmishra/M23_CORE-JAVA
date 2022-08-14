af131b035e4523f0ae8878061a635724fa0f296d
Ritesh Mishra <96378085+iriteshmishra@users.noreply.github.com>
2022-08-14 23:08:55 +0530

    First Commit

---
+++ Assignment - 3/src/com/cg/string/Example2.java
@@ -0,0 +1,14 @@
+package com.cg.string;
+
+import java.util.Scanner;
+
+public class Example2 {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		String str=new String(sc.nextLine());
+		System.out.println(str);
+		sc.close();
+	}
+
+}
---
+++ Assignment - 3/src/com/cg/string/Example3.java
@@ -0,0 +1,15 @@
+package com.cg.string;
+
+public class Example3 {
+
+	public static void main(String[] args) {
+	
+		String str=new String("Hello");
+		String str1=new String("Hello");
+		String str2=new String("Hello");
+		System.out.println(str1==str2);
+		System.out.println(str1.equals(str2));
+
+	}
+
+}
