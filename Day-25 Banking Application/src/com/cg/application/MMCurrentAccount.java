da1cfd403a6a4712e6b2646d1aea1dbd4e5aaae6
Ritesh Mishra <96378085+iriteshmishra@users.noreply.github.com>
2022-09-07 20:40:54 +0530

    First Commit

---
+++ Day-25 Banking Application/src/com/cg/application/MMCurrentAccount.java
@@ -0,0 +1,18 @@
+package com.cg.application;
+
+import com.cg.framework.CurrentAccount;
+
+public class MMCurrentAccount extends CurrentAccount
+{
+
+	public MMCurrentAccount(int accNo, String accNm, float accBal, float creditLimit) {
+		super(accNo, accNm, accBal, creditLimit);
+		// Auto-generated constructor stub
+	}
+
+	@Override
+	public String toString() {
+		return String.format("MMCurrentAccount [toString()=%s]", super.toString());
+	}
+	
+}
---
+++ Day-25 Banking Application/src/com/cg/framework/BankFactory.java
@@ -0,0 +1,15 @@
+package com.cg.framework;
+
+public class BankFactory {
+
+	public SavingAccount getNewSavingAccount(int accNo, String accNm, float accBal, boolean isSalaried) {
+		// Auto-generated method stub
+		return null;
+	}
+
+	public CurrentAccount getNewCurrentAccount(int accNo, String accNm, float accBal, float creditLimit) {
+		// Auto-generated method stub
+		return null;
+	}
+
+}
